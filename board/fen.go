66abd4f7dd66e6e06a14b97e1eb9153b02eee24b
Hamza El <hamza.elbouatmani@gmail.com>
2020-06-08 22:58:18 +0100

    Implement fenIsValid

--- board/fen.go
+++ board/fen.go
@@ -1,5 +1,13 @@
 package board
 
+import (
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 // FromFen sets the state of the board
 // to the state given by the FEN representation
 func (b Board) FromFen(fen string) {
@@ -10,5 +18,42 @@ func (b Board) FromFen(fen string) {
 }
 
 func fenIsValid(fen string) bool {
-	return false
+	p := "^((?i)[pnbrqk0-9]*/){7}((?i)[pnbrqk0-9])* [wb] ((?i)[kq-])* [a-h0-8-]* [0-9]* [0-9]*$"
+	r, err := regexp.Compile(p)
+	if err != nil {
+		log.Print("Regex could not compile")
+		return false
+	}
+	if match := r.MatchString(fen); !match {
+		log.Print("Fen did not match Regex pattern structure")
+		return false
+	}
+	parts := strings.Split(fen, " ")
+	ranks := strings.Split(parts[0], "/")
+	if len(ranks) != 8 {
+		log.Print("Number of ranks is not 8")
+		return false
+	}
+
+	for i, rank := range ranks {
+		// Check number of overall pieces
+		nbrEmpty := 0
+		nbrPieces := 0
+		for _, r := range rank {
+			if unicode.IsDigit(r) {
+				// It's a number of consecutive empty spaces
+				n, _ := strconv.Atoi(string(r))
+				nbrEmpty += n
+			} else {
+				// It's a piece
+				nbrPieces += 1
+			}
+		}
+		if nbrEmpty+nbrPieces > 8 {
+			log.Printf("Rank %d (%s)contains more than 8 squares! (%d pieces and %d empty)", i+1, rank, nbrPieces, nbrEmpty)
+			return false
+		}
+
+	}
+	return true
 }
--- board/fen_test.go
+++ board/fen_test.go
@@ -16,9 +16,11 @@ func TestFen_fenIsValid(t *testing.T) {
 		"rnbq1rk1/pppp2pp/5n2/8/1bPPPp2/2NQ4/PP3PBP/R1B1K1NR b KQ e3 0 8",
 	}
 	invalidFens := map[string]string{
+		"Wrong Format: missing player":              "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR KQkq - 0 1",
+		"Wrong Format: missing en passant square":   "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq 0 1",
+		"Wrong format: missing one rank":            "rnbqkbnr/pppppppp/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
 		"Wrong Half Clock":                          "rnbqkrnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - X 1",
 		"Wrong Full Clock":                          "rnbqkrnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 Z",
-		"3 Rooks in 8th rank":                       "rnbqkrnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
 		"Wrong player to move":                      "r1bq1rk1/ppppn1pp/5n2/5p2/1bPP4/2NQ3N/PP1BPPBP/R3K2R x KQ - 9 9",
 		"Wrong number of empty squares in 5th rank": "r1bq1rk1/pppp2pp/5nn1/5p3/2PP4/2PQ3N/P2BPPBP/R3K1R1 b Q - 2 11",
 		"Wrong Castling rights":                     "rnbqkr2/pppp2pp/5n2/5p2/1bPP4/2NQ4/PP2PP1P/R1B1KBNR w KRq - 4 7",
