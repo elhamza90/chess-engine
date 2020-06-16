7845fdd2ef09377c517802826b9477553f462701
Hamza El <hamza.elbouatmani@gmail.com>
2020-06-16 22:52:29 +0100

    Complete Tests for location pieces

--- board/board_test.go
+++ board/board_test.go
@@ -47,8 +47,22 @@ func TestBoard_FromFen(t *testing.T) {
 				epSquare: uint64(262144),
 			},
 			Pieces: bbRepr{
-				White: map[Piece]uint64{},
-				Black: map[Piece]uint64{},
+				White: map[Piece]uint64{
+					'P': uint64(557878784),
+					'R': uint64(33),
+					'N': uint64(266240),
+					'B': uint64(16388),
+					'K': uint64(8),
+					'Q': uint64(16),
+				},
+				Black: map[Piece]uint64{
+					'P': uint64(56013623713071104),
+					'R': uint64(9295429630892703744),
+					'N': uint64(144150372447944704),
+					'B': uint64(292733975779082240),
+					'K': uint64(2251799813685248),
+					'Q': uint64(1152921504606846976),
+				},
 				Empty: uint64(7503243165898056130),
 			},
 		},
@@ -63,8 +77,22 @@ func TestBoard_FromFen(t *testing.T) {
 				epSquare: 0,
 			},
 			Pieces: bbRepr{
-				White: map[Piece]uint64{},
-				Black: map[Piece]uint64{},
+				White: map[Piece]uint64{
+					'P': uint64(34359799552),
+					'R': uint64(129),
+					'N': uint64(66),
+					'B': uint64(36),
+					'K': uint64(8),
+					'Q': uint64(16),
+				},
+				Black: map[Piece]uint64{
+					'P': uint64(69524319247532032),
+					'R': uint64(9295429630892703744),
+					'N': uint64(4755801206503243776),
+					'B': uint64(2305843010287435776),
+					'K': uint64(576460752303423488),
+					'Q': uint64(1152921504606846976),
+				},
 				Empty: uint64(290763615508566016),
 			},
 		},
@@ -97,14 +125,14 @@ func TestBoard_FromFen(t *testing.T) {
 			for p, expectedPos := range expected.Pieces.White {
 				resultPos := b.Pieces.White[p]
 				if expectedPos != resultPos {
-					t.Errorf("Error setting White Piece position. Expected %b but got %b", expectedPos, resultPos)
+					t.Errorf("Error setting White Piece %c position. Expected %b but got %b", p, expectedPos, resultPos)
 				}
 			}
 
 			for p, expectedPos := range expected.Pieces.Black {
 				resultPos := b.Pieces.Black[p]
 				if expectedPos != resultPos {
-					t.Errorf("Error setting Black Piece position. Expected %b but got %b", expectedPos, resultPos)
+					t.Errorf("Error setting Black Piece %c position. Expected %b but got %b", p, expectedPos, resultPos)
 				}
 			}
 
