e6dd84529c96d87978384100e1404daf02da2127
Hamza El <hamza.elbouatmani@gmail.com>
2020-06-16 22:13:13 +0100

    Refactor setting piece locations in bitboard

--- board/board.go
+++ board/board.go
@@ -30,6 +30,44 @@ func fenCastleRights(s string) (castleRights, castleRights) {
 	return castleRights(w_castling), castleRights(b_castling)
 }
 
+func fenToBitboardPieces(fen string) (map[Piece]uint64, map[Piece]uint64) {
+	blackMap := map[Piece]uint64{}
+	whiteMap := map[Piece]uint64{}
+	//log.Printf("Pieces: %s", parts[0])
+	var (
+		ix    int    = 0
+		inc   int    = 0
+		pos   uint64 = 0
+		piece Piece
+	)
+	for i := len(fen) - 1; i >= 0; i-- {
+		c := rune(fen[i])
+		if c != '/' {
+			//log.Print(string(c), ix)
+			if unicode.IsDigit(c) {
+				// Get number of empty squares
+				inc, _ = strconv.Atoi(string(c))
+				//log.Printf("There are %d zeros in the %dth index", nbrZeros, ix)
+			} else {
+				inc = 1
+				// Check type and color of piece
+				piece = Piece(unicode.ToUpper(c))
+				pos = uint64(1) << ix
+				if unicode.IsUpper(c) {
+					whiteMap[piece] |= pos
+				} else {
+					blackMap[piece] |= pos
+				}
+
+			}
+			// increment only when there is no separator
+			// and by number of empty squares or by one piece
+			ix += inc
+		}
+	}
+	return whiteMap, blackMap
+}
+
 // FromFen sets the state of the board
 // to the state given by the FEN representation
 func (b *Board) FromFen(fen string) {
@@ -59,47 +97,18 @@ func (b *Board) FromFen(fen string) {
 	}
 
 	// Set Pieces locations in Bitboards
+	b.Pieces.White, b.Pieces.Black = fenToBitboardPieces(parts[0])
 
-	b.Pieces.Black = map[Piece]uint64{}
-	b.Pieces.White = map[Piece]uint64{}
-	res := uint64(0)
-	//log.Printf("Pieces: %s", parts[0])
-	var (
-		ix       int = 0
-		nbrZeros int = 0
-		inc      int = 0
-	)
-	for i := len(parts[0]) - 1; i >= 0; i-- {
-		c := rune(parts[0][i])
-		if c != '/' {
-			//log.Print(string(c), ix)
-			if unicode.IsDigit(c) {
-				// set "c" ones in the current index going to the left
-				nbrZeros, _ = strconv.Atoi(string(c))
-				for ii := 0; ii < nbrZeros; ii++ {
-					res |= (uint64(1) << (ix + ii))
-				}
-				inc = nbrZeros
-				//log.Printf("There are %d zeros in the %dth index", nbrZeros, ix)
-				//log.Printf("%b", res)
-			} else {
-				inc = 1
-				// Check type and color of piece
-				p := Piece(unicode.ToUpper(c))
-				pos := uint64(1) << ix
-				if unicode.IsUpper(c) {
-					b.Pieces.White[p] |= pos
-				} else {
-					b.Pieces.Black[p] |= pos
-				}
-
-			}
-			// increment only when there is no separator
-			// and by number of empty squares or by one piece
-			ix += inc
-		}
+	// Calculate Empty Squares from pieces locations
+	var occupied uint64 = 0
+	for _, pos := range b.Pieces.Black {
+		occupied += pos
 	}
-	b.Pieces.Empty = res
+	for _, pos := range b.Pieces.White {
+		occupied += pos
+	}
+	b.Pieces.Empty = ^occupied
+
 }
 
 // InitStandard sets the board
